58af1d993af31569b5ac14bd9dbd9e910f833539
Simone Mauro <simon.mauro@virgilio.it>
2023-12-25 19:09:32 +0100

    Update colori dark mode e inizio test immagini

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ let toggle = document.getElementById("toggle");
 
 toggle_checkbox.addEventListener("change", function(){
 
-    if(toggle_checkbox.checked){
+    
+    if(toggle_checkbox.checked){ //se la checkbox passa a checked una volta cambiata
         toggle.style.left="30px";
         toggle.innerHTML= '<i class="bi bi-moon-stars"></i>';
         document.getElementById("navbar").setAttribute("data-bs-theme", "dark");
-    }else{
+        document.querySelector("header .ms_toggle label").style.backgroundColor = "#222222"; //cambio colore dello sfondo del toggle
+    }else{ //se la checkbox passa a unchecked una volta cambiata
         toggle.style.left="0";
         toggle.innerHTML= '<i class="bi bi-brightness-high"></i>'
         document.getElementById("navbar").setAttribute("data-bs-theme", "");
+        document.querySelector("header .ms_toggle label").style.backgroundColor = "#cfcfcf";
     }
 
     body.classList.toggle("dark-mode");
