a9326e59fb84e1d92938386a901eee73cd0382fb
Simone Mauro <simon.mauro@virgilio.it>
2024-01-25 15:33:27 +0100

    miglioramento JS dell'evento di scroll

--- main.js
+++ main.js
@@ -67,28 +67,33 @@ Fancybox.bind('[data-fancybox]', {
 //configurazione animazioni con lo scroll della pagina
 let sections = document.querySelectorAll(".ms_animation");
 
-sections[0].classList.add("animating");
+
+//richiamo la funzione dedicata sia all'evento di scroll che all'avvio/refresh della pagina
+scrollAnimation();
+
+window.addEventListener("scroll", scrollAnimation);
 
 
-window.addEventListener("scroll", function(){
+//funzione che gestisce l'animazione di scroll delle sezioni in base all'altezza della finestra del browser
+function scrollAnimation(){
 
     //altezza della finestra
-    let innerHeight = this.window.innerHeight;
+    let innerHeight = window.innerHeight;
 
     sections.forEach(section => {
 
         //coordinata top della singola sezione
         let sectionHeight = section.getBoundingClientRect().top;
 
-        //se l'altezza della finestra supera la cima
+        //se l'altezza della finestra supera la cima, allora aggiungo la classe che fa scattare l'animazione
         if(sectionHeight < innerHeight - 100){
             section.classList.add("animating");    
         }
     })
+}
 
-})
 
-
+//VECCHIA FUNZIONE DI SCROLL
 /* window.onscroll = () => {
     sections.forEach (section => {
         let top = window.scrollY;
@@ -102,7 +107,5 @@ window.addEventListener("scroll", function(){
             section.classList.add("animating");
         }
     })
-
-
 } */
 
